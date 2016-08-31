fbe9b1435e14b98107fa306f274d4ae7a5dfce99
istinson <ian.c.stinson@gmail.com>
2016-08-31 16:49:05 -0700

    [feat] cloudflare script

---
+++ cloudflare.js
@@ -0,0 +1,30 @@
+var jobWatcher = require('./jobWatcher');
+
+function cloudFlareEval() {
+	var items = Array.from(document.getElementsByClassName('js-job-container'));
+	return items.map(function(item) {
+		return {
+			title: item.firstElementChild.firstElementChild.firstElementChild.innerText,
+			loc: item.firstElementChild.lastElementChild.innerText,
+			link: item.firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute('href'),
+			department: item.parentNode.firstElementChild.firstElementChild.firstElementChild.innerText
+		};
+	});
+}
+
+function cloudFlareFilter(item) {
+  if (item.loc === 'SAN FRANCISCO, CA' && item.department === 'ENGINEERING') {
+	  return true;
+  }
+}
+
+function cloudFlareParser(item) {
+	return {
+		id: `cloudflare${item.link.slice(-22)}`,
+		title: item.title,
+		loc: item.loc,
+		link: `https://careers.jobscore.com${item.link}`
+	}
+}
+
+jobWatcher.watch('https://careers.jobscore.com/careers/cloudflare', cloudFlareEval, cloudFlareFilter, cloudFlareParser, './cloudFlare.json');
