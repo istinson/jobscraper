9a44db119e3d0f565847978681f06147b3db3248
istinson <ian.c.stinson@gmail.com>
2016-09-08 15:11:50 -0700

    [feat] slack refactor to nightmare

---
+++ scripts/slack.js
@@ -0,0 +1,31 @@
+var jobWatcher = require('./../jobWatcher');
+
+function slackEval() {
+	var items = Array.from(document.getElementsByTagName('h4'));
+	return items.map(function (item) {
+		return {
+			title: item.innerText,
+			link: `https://slack.com${item.parentNode.getAttribute('href')}`,
+			loc: item.parentNode.parentNode.getAttribute('data-location'),
+			department: item.parentNode.parentNode.parentNode.parentNode.firstElementChild.getAttribute('id')
+		};
+	});
+}
+
+function slackFilter(item) {
+	if (item.department === 'engineering' && item.loc.indexOf('San Francisco') > -1) {
+		return true;
+	}
+}
+
+function slackParser(item) {
+	return {
+		id: `slack${item.link.slice(23, 29)}`,
+		title: item.title,
+		link: item.link,
+		loc: item.loc
+	};
+}
+
+jobWatcher.watch('https://slack.com/jobs/dept/engineering', slackEval,
+	slackFilter, slackParser, './json/slack.json');
