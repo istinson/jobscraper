c63f0865d9cd119e228c310da8e95be851c4e029
istinson <ian.c.stinson@gmail.com>
2016-10-21 16:55:15 -0700

    [fix] create app module with handler function

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const config = require('./config');
 const Jobs = require('./models/jobModel');
 mongoose.connect(config.getDbConnectionString());
 
-require('./scripts/twitch')
-  .then(function(results) {
-	  results.forEach((job, index, arr) => {
-	  	Jobs.findByIdAndUpdate(job._id, job, {upsert: true, setDefaultsOnInsert: true}, function (err, doc) {
-	  		if (err) {
-	  			throw err;
-	  		} else if (index === arr.length - 1 && doc){
-				  mongoose.disconnect();
-	  		}
-	  	});
-	  });
-  })
-  .catch(function(error) {
-	  throw error;
-  });
+module.exports = {
+	handler: function() {
+		require('./scripts/twitch')
+		.then(function(results) {
+			results.forEach((job, index, arr) => {
+				Jobs.findByIdAndUpdate(job._id, job, {upsert: true, setDefaultsOnInsert: true}, function (err, doc) {
+					if (err) {
+						throw err;
+					} else if (index === arr.length - 1 && doc){
+						mongoose.disconnect();
+					}
+				});
+			});
+		})
+		.catch(function(error) {
+			throw error;
+		});
+	}
+};
+
